0091a25c5d4ed9aa97cd081608501d26574c4fec
IamBlueSlime <iamblueslime@gmail.com>
2015-08-12 15:13:21 +0200

    GRAAAADLE POWEERR

--- src/main/java/fr/blueslime/hangover/arena/ArenaManager.java
+++ src/main/java/fr/blueslime/hangover/arena/ArenaManager.java
@@ -3,6 +3,7 @@ package fr.blueslime.hangover.arena;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
+import net.samagames.api.SamaGamesAPI;
 import net.samagames.tools.LocationUtils;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
@@ -16,26 +17,14 @@ public class ArenaManager
 {
 	public Arena loadArena()
     {
-        File worldFolder = Bukkit.getWorlds().get(0).getWorldFolder();
-        File arenaFile = new File(worldFolder, "arena.json");
+        JsonObject jsonArena = SamaGamesAPI.get().getGameManager().getGameProperties().getConfigs();
 
-        try
-        {
-            JsonObject jsonArena = new JsonParser().parse(new FileReader(arenaFile)).getAsJsonObject();
+        JsonArray jsonCauldrons = jsonArena.get("cauldrons").getAsJsonArray();
+        ArrayList<Location> cauldrons = new ArrayList<>();
 
-            JsonArray jsonCauldrons = jsonArena.get("cauldrons").getAsJsonArray();
-            ArrayList<Location> cauldrons = new ArrayList<>();
+        for(int i = 0; i < jsonCauldrons.size(); i++)
+            cauldrons.add(LocationUtils.str2loc(jsonCauldrons.get(i).getAsString()));
 
-            for(int i = 0; i < jsonCauldrons.size(); i++)
-                cauldrons.add(LocationUtils.str2loc(jsonCauldrons.get(i).getAsString()));
-
-            return new Arena(LocationUtils.str2loc(jsonArena.get("spawn").getAsString()), cauldrons);
-        }
-        catch (FileNotFoundException e)
-        {
-            e.printStackTrace();
-        }
-
-        return null;
+        return new Arena(LocationUtils.str2loc(jsonArena.get("spawn").getAsString()), cauldrons);
 	}
 }
