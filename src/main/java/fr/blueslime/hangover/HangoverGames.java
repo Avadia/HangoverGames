5142e346670299884f75f9ec91f4da93d3075a78
BlueSlime <jeremylevilain78@orange.fr>
2017-08-23 23:12:57 +0200

    Publishing source code.

--- src/main/java/fr/blueslime/hangover/HangoverGames.java
+++ src/main/java/fr/blueslime/hangover/HangoverGames.java
@@ -7,6 +7,22 @@ import net.samagames.api.SamaGamesAPI;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
+/*
+ * This file is part of HangoverGames.
+ *
+ * HangoverGames is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * HangoverGames is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with HangoverGames.  If not, see <http://www.gnu.org/licenses/>.
+ */
 public class HangoverGames extends JavaPlugin
 {
     private static HangoverGames instance;
